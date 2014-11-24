e1209c822eb6bd4e78d6d955d9f230c650bd3033
Saverio Giallorenzo <thesave86@gmail.com>
2014-11-24 14:56:52 +0000

    updated js library
    Former-commit-id: e7f68c23a078a3dae755ac3ef312b34182873789

---
+++ www/js/site.js
@@ -0,0 +1,19 @@
+/* global $:false, window:false, document:false, alert:false, 
+SyntaxHighlighter:false */
+"use strict";
+
+var extLang = {
+  "ext": {
+    "xml": { "lang": "xml" },
+    "html": { "lang": "xml" },
+    "json": { "lang": "jscript" },
+    "js": {"lang": "jscript" },
+    "ol": { "lang": "jolie" },
+    "iol": { "lang": "jolie" },
+    "java": { "lang": "java" },
+    "txt": { "lang": "plain" }
+  }
+};
+
+// Loaded on start
+$( document ).ready();
