24b700779c697139a184b2a7693c43652c056cf4
thesave <saverio.giallorenzo@gmail.com>
2015-03-16 20:54:45 +0100

    fixed missing ids for titles

--- docs/site_builder/src/org/pegdownService.java
+++ docs/site_builder/src/org/pegdownService.java
@@ -1,14 +1,22 @@
 package org;
 
+import static jodd.jerry.Jerry.jerry;
+import jodd.jerry.Jerry;
 import jolie.runtime.JavaService;
+
+import org.pegdown.Parser;
 import org.pegdown.PegDownProcessor;
 
 public class pegdownService extends JavaService{
 	
-	private static final PegDownProcessor proc = new PegDownProcessor();
+	private static final PegDownProcessor proc = new PegDownProcessor( Parser.ALL );
 	
 	public String markdownToHtml( String text ){
-				return proc.markdownToHtml( text );
+		Jerry doc = jerry( proc.markdownToHtml( text ) );
+    for( Jerry node: doc.$( "h1,h2,h3,h4,h5" ) ){
+    	node.attr( "id", node.text() );
+    }
+    return doc.html();
 	}
 	
 }
