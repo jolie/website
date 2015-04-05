a57be2ce8d831acd7cb47556401c2f38367a74d1
thesave <saverio.giallorenzo@gmail.com>
2015-04-05 23:55:29 +0200

    fixed headers id creation

--- docs/site_builder/src/org/pegdownService.java
+++ docs/site_builder/src/org/pegdownService.java
@@ -9,12 +9,18 @@ import org.pegdown.PegDownProcessor;
 
 public class pegdownService extends JavaService{
 	
-	private static final PegDownProcessor proc = new PegDownProcessor( Parser.ALL );
+	private static final PegDownProcessor proc = new PegDownProcessor( Parser.ABBREVIATIONS +
+						Parser.AUTOLINKS + Parser.DEFINITIONS +
+						Parser.HARDWRAPS + Parser.TABLES + 
+						Parser.FENCED_CODE_BLOCKS +
+						Parser.STRIKETHROUGH );
 	
-	public String markdownToHtml( String text ){
+	public String markdownToHtml( String text ) {
 		Jerry doc = jerry( proc.markdownToHtml( text ) );
     for( Jerry node: doc.$( "h1,h2,h3,h4,h5" ) ){
-	node.attr( "id", node.text().toLowerCase().replace(" ","-") );
+    	String id = node.text().toLowerCase()
+    		.replaceAll("[^a-zA-Z0-9 -]" , "").replaceAll( " ", "-" );
+			node.attr( "id", id );
     }
     return doc.html();
 	}
